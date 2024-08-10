40e31c80e95d5b8c973a3b94c7e93957c3457843
LMF3z <moisesfreites3@gmail.com>
2024-08-10 12:39:25 -0400

    Finalizacion de seccion 13 lazy loading routes & children routes

---
+++ heroesApp/src/app/heroes/pages/search-page/search-page.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shared-page',
+  templateUrl: './search-page.component.html',
+  styles: ``,
+})
+export class SearchPageComponent {}
